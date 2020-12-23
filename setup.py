196801f7a4885596babb4c2f9a53940f5d7cce43
Matthew R. Becker <becker.mr@gmail.com>
2020-12-23 14:51:24 -0600

    ENH added first function for tracts

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="fsi_tools",
+    version="0.1.0",
+    packages=find_packages(),
+    include_package_data=True,
+    author='Matthew R. Becker',
+    author_email='becker.mr@gmail.com',
+    url='https://github.com/LSSTDESC/fsi-tools',
+)
